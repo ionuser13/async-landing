c1194fa7bffc997c2b6f23aeda493ebae4e2ca89
userjohn <johnchacpi479@gmail.com>
2022-10-18 23:25:20 -0500

    minor changes

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,15 +1,15 @@
-const API = 'https://youtube-v31.p.rapidapi.com/playlistItems?playlistId=PLn5jU74I0Hxd1OdqbHO30SmYXphTYvy61&part=snippet&maxResults=5';
+const API = 'https://youtube-v31.p.rapidapi.com/playlistItems?playlistId=PLjT6ePOFLFf3gHO_fXXmikcipOV3ZLYB0&part=snippet&maxResults=5';
 
 const content = null || document.getElementById("content");
 
 const options = {
 	method: 'GET',
 	headers: {
+		'X-CACHEBYPASS': ' ',
 		'X-RapidAPI-Key': 'f795c979d9msh737fce69d2d0e2ap140ed4jsn705b07bcd026',
 		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
 	}
 };
-
 async function fetchData(urlApi){//transforms data into an iterable object
     const response = await fetch(urlApi, options);
     const data = await response.json();
@@ -25,7 +25,7 @@ async function fetchData(urlApi){//transforms data into an iterable object
         ${videos.items.map(video => `
             <div class="group relative">
                 <div class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-                    <img src="${video.snippet.thumbnail.high.url}" alt="${video.snippet.description}" class="w-full">
+                    <img src="${video.snippet.thumbnails    .high.url}" alt="${video.snippet.description}" class="w-full">
                 </div>
                 <div class="mt-4 flex justify-between">
                     <h3 class="text-sm text-gray-700">
@@ -36,9 +36,10 @@ async function fetchData(urlApi){//transforms data into an iterable object
             </div>
         `).slice(0, 4).join("")}
         `;
+        content.innerHTML = view;
      }
-     catch{
-
+     catch (error){
+        content.innerHTML = `An error has ocurred: ${error}`
      }
 })();
 
