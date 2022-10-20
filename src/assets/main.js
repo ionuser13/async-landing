9e60a7b5c51cf9e1a4584db3dc2645feffcd636e
userjohn <johnchacpi479@gmail.com>
2022-10-19 21:29:46 -0500

    latest tweets added

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,6 +1,8 @@
-const API = "https://youtube-v31.p.rapidapi.com/playlistItems?playlistId=PLn5jU74I0Hxd1OdqbHO30SmYXphTYvy61&part=snippet&maxResults=5";
+const playlistAPI = "https://youtube-v31.p.rapidapi.com/playlistItems?playlistId=PLn5jU74I0Hxd1OdqbHO30SmYXphTYvy61&part=snippet&maxResults=10";
+const twtAPI = "https://twitter154.p.rapidapi.com/user/tweets?username=ionuser03&limit=40"
 
-const content = null || document.getElementById("content");
+const videosContainer = null || document.getElementById("videos");
+const twitter = null || document.getElementById("twitter") 
 
 const options = {
 	method: 'GET',
@@ -9,8 +11,21 @@ const options = {
 		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
 	}
 };
+
+const optionsTwt = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'f795c979d9msh737fce69d2d0e2ap140ed4jsn705b07bcd026',
+		'X-RapidAPI-Host': 'twitter154.p.rapidapi.com'
+	}
+};
 async function fetchData(urlApi){//transforms data into an iterable object
     const response = await fetch(urlApi, options);
+    const data = await response.json();
+    return data;
+}
+async function fetchData2(urlApi){//transforms data into an iterable object
+    const response = await fetch(urlApi, optionsTwt);
     const data = await response.json();
     return data;
 }
@@ -18,10 +33,11 @@ async function fetchData(urlApi){//transforms data into an iterable object
 //recursive function
 (async() => {
      try{
-        const videos = await fetchData(API);
+        const videos = await fetchData(playlistAPI);
         //generate a template to iterate through all the elements
         let view = `
-        ${videos.items.map((video) => `
+        ${videos.items.map(video => `
+        <a href="https://www.youtube.com/watch?v=${video.snippet.resourceId.videoId}&list=${video.snippet.playlistId}&index=${video.snippet.position + 1}" target="_blank">
             <div class="group relative">
                 <div class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
                     <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
@@ -33,16 +49,41 @@ async function fetchData(urlApi){//transforms data into an iterable object
                     </h3>
                 </div>
             </div>
+            </a>
         `).slice(0, 8).join("")}
         `;
-        content.innerHTML = view;
+        videosContainer.innerHTML = view;
      }
      catch (error){
         console.error(error)
-        content.innerHTML = `An error has ocurred: ${error}`
+        videosContainer.innerHTML = `An error has ocurred: ${error}`
      }
 })();
-
+(async() => {
+    try{
+        const tweets = await fetchData2(twtAPI);
+        let post = `
+        ${tweets.results.map(tweet => `
+        <div class="group relative">
+            <div class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+                <img src="${tweet.user.profile_pic_url}" alt="${tweet.user.description}" class="w-full">
+            </div>
+            <div class="mt-4 flex justify-between">
+                <h3 class="text-sm text-gray-700">
+                    <span aria-hidden="true" class="absolute inset-0"></span>
+                    ${tweet.text}
+                </h3>
+            </div>
+        </div>
+        `).slice(0, 8).join("")}
+        `;
+        twitter.innerHTML = post;
+    }
+    catch(error){
+        console.error(error)
+        twitter.innerHTML = `An error has ocurred: ${error}`
+    }
+})();
 
 
 // fetch(API, options)
